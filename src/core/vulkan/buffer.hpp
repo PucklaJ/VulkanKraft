284bc4c0117e59859049f393b1790b5819527bec
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2021-08-13 11:45:26 +0200

    Add comments to core/vulkan/buffer.hpp

--- src/core/vulkan/buffer.hpp
+++ src/core/vulkan/buffer.hpp
@@ -4,15 +4,19 @@
 
 namespace core {
 namespace vulkan {
+// Represents an allocated amount of memory used for a buffer
 class Buffer {
 public:
+  // Initialise the buffer and creates it if data is not nullptr
   Buffer(const Context &context, vk::BufferUsageFlags usage,
          const size_t buffer_size, const void *data = nullptr);
   Buffer(Buffer &&rhs);
   ~Buffer();
 
+  // Update the data of the buffer. Recreate it if the size changed
   void set_data(const void *data, const size_t data_size,
                 const size_t offset = 0);
+  // Bind the buffer depending on the usage
   void bind(const RenderCall &render_call) const;
   inline const vk::Buffer &get_handle() const { return m_handle; }
 
