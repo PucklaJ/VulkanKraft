abb0604f59ffd5298b1064e8e30d59f6219272e1
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2021-08-18 13:24:10 +0200

    Add comments to core/text/text.hpp

--- src/core/text/text.hpp
+++ src/core/text/text.hpp
@@ -12,25 +12,36 @@
 
 namespace core {
 namespace text {
+// This represents a 2D text rendered to the screen
 class Text {
 public:
+  // A value used by shader which unfortunately limits how much text objects can
+  // be used at the same time
   static constexpr uint32_t max_texts = 20;
 
+  // Holds all global uniforms used in the vertex shader
   struct GlobalUniform {
     glm::mat4 proj;
   };
 
+  // The Vertex used by the text shader
   class Vertex {
   public:
     Vertex() = default;
+    // lt ..... left top position of the rectangle
     Vertex(const glm::vec2 &lt, glm::f32 x, glm::f32 y, glm::f32 u, glm::f32 v)
         : position(lt.x + x, lt.y + y), uv(u, v) {}
 
     glm::vec2 position;
     glm::vec2 uv;
   };
+
+  // The mesh used for the text objects will always hold six vertices
   typedef std::array<Vertex, 6> Mesh;
 
+  // font ..... The font which should be used by this text object
+  // string ... The initial text which should be displayed
+  // position . The initial position
   Text(const vulkan::Context &context, Shader &shader, Font &font,
        const std::wstring &string, const glm::vec2 &position = glm::vec2(),
        const float font_size = 50.0f);
@@ -42,15 +53,19 @@ public:
   inline const uint32_t &get_width() const { return m_texture_width; }
   inline const uint32_t &get_height() const { return m_texture_height; }
 
+  // Renders the text object to the screen
   void render(const vulkan::RenderCall &render_call);
 
 private:
+  // Holds the new mesh and which swap image indices still need the update
   struct BufferWrite {
     Mesh mesh;
     std::set<uint32_t> image_indices;
   };
 
+  // Creates a new texture from the given string and font_size
   Texture _build_texture();
+  // Creates a new mesh from the given position and texture size
   void _build_buffers();
 
   const vulkan::Context &m_context;
@@ -62,6 +77,7 @@ private:
   glm::vec2 m_position;
 
   Texture m_text_texture;
+  // There is a buffer for every swap chain image
   std::vector<vulkan::Buffer> m_buffers;
   uint32_t m_texture_width;
   uint32_t m_texture_height;
