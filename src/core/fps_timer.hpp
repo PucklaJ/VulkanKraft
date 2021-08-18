0262131df5f9d717f2e30a40babffe7dbce0f004
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2021-08-18 13:12:15 +0200

    Add comments to core/fps_timer.hpp

--- src/core/fps_timer.hpp
+++ src/core/fps_timer.hpp
@@ -2,8 +2,11 @@
 #include "settings.hpp"
 
 namespace core {
+// Calculates the elapsed time from one time point to another and locks the
+// frame time
 class FPSTimer {
 public:
+  // The destructor is utilised to get the end time of a frame
   class DeltaTimer {
   public:
     DeltaTimer(FPSTimer &parent);
@@ -11,21 +14,28 @@ public:
 
   private:
     FPSTimer &m_parent;
+    // When did the frame start in seconds
     const float m_start_time;
   };
 
   friend class DeltaTimer;
 
+  // max_fps .... The frame is locked to hold a steady fps count
   FPSTimer(const size_t max_fps);
 
+  // Returns the time elapsed between the beginning and end of the last frame
   inline const float &get_delta_time() const { return m_delta_time; }
 
+  // Starts a frame and returns a DeltaTimer which is used to end the frame
+  // automatically when its scope ends
   DeltaTimer begin_frame();
 
 private:
+  // Handles everything that happens when a frame ends and locks the frame time
   void _end_frame();
 
   float m_delta_time;
+  // At which frame time get the FPS locked in seconds
   const float m_min_delta_time;
 };
 } // namespace core
