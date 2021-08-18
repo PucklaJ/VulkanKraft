7d2f2bf03fc077413c6ca68a24251eee3f0340bc
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2021-08-18 11:11:04 +0200

    Fix having weird frame time on windows

--- src/core/fps_timer.cpp
+++ src/core/fps_timer.cpp
@@ -23,9 +23,17 @@ FPSTimer::DeltaTimer FPSTimer::begin_frame() {
 void FPSTimer::_end_frame() {
   const auto start_time{static_cast<float>(glfwGetTime())};
   if (m_delta_time < m_min_delta_time) {
+    const auto sleep_time{m_min_delta_time - m_delta_time};
+    // Sleep is unreliable on windows. TODO: Implement vertical sync
+#ifdef _WIN32
+    while (static_cast<float>(glfwGetTime()) < start_time + sleep_time)
+      ;
+#else
     std::this_thread::sleep_for(std::chrono::microseconds{
-        static_cast<long int>((m_min_delta_time - m_delta_time) * 1000000.0f)});
+        static_cast<long int>(sleep_time * 1000000.0f)});
+#endif
   }
-  m_delta_time += static_cast<float>(glfwGetTime()) - start_time;
+  const auto bonus_time{static_cast<float>(glfwGetTime()) - start_time};
+  m_delta_time += bonus_time;
 }
 } // namespace core
