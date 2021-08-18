a560db0600374dbc3774e3a705960a977e9110af
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2021-08-18 13:05:39 +0200

    Add comments to core/render_2d.hpp

--- src/core/render_2d.hpp
+++ src/core/render_2d.hpp
@@ -5,12 +5,17 @@
 #include <glm/glm.hpp>
 
 namespace core {
+// An object that handles the rendering of a texture as a 2D square
 class Render2D {
 public:
+  // Needs to be called before using any objects of this class
+  // This needs to be the texture 2d shader
   static inline void set_shader(Shader &shader) { m_shader = &shader; }
+  // Binds the texture 2d shader
   static inline void bind_shader(const vulkan::RenderCall &render_call) {
     m_shader->bind(render_call);
   }
+  // Calculates the orthographic projection for 2d and uploads it to the GPU
   static glm::mat4
   update_projection_matrix(const uint32_t window_width,
                            const uint32_t window_height,
@@ -18,8 +23,10 @@ public:
 
   Render2D(Texture &texture);
 
+  // Updates the model matrix
   void set_model_matrix(const glm::vec2 &position = glm::vec2(0.0f, 0.0f),
                         const glm::vec2 &scale = glm::vec2(1.0f, 1.0f));
+  // Renders the texture to the screen
   void render(const vulkan::RenderCall &render_call);
 
 private:
