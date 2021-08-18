7919dc406b61d7e7e55d916e32490739040f4c59
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2021-08-18 13:07:55 +0200

    Add comments to core/log.hpp

--- src/core/log.hpp
+++ src/core/log.hpp
@@ -3,25 +3,31 @@
 #include <string>
 
 namespace core {
+// Handles logging to stdout
 class Log {
 public:
+  // Prints a general information string to stdout
   static inline void info(const std::string &msg) {
     _log(info_type, std::cout, msg);
   }
+  // Prints a warning string to stderr
   static inline void warning(const std::string &msg) {
     _log(warning_type, std::cerr, msg);
   }
+  // Prints an error string to stderr
   static inline void error(const std::string &msg) {
     _log(error_type, std::cerr, msg);
   }
 
 private:
+  // Variables used to decorate the printed message
   static constexpr char start_type[] = "[";
   static constexpr char end_type[] = "] ";
   static constexpr char info_type[] = "\033[1;34mINFO\033[0m";
   static constexpr char warning_type[] = "\033[1;33mWARN\033[0m";
   static constexpr char error_type[] = "\033[1;31mERROR\033[0m";
 
+  // Prints msg to stream using msg_type as decoration
   static void _log(const char *msg_type, std::ostream &stream,
                    const std::string &msg);
 };
